9f6c13bcbe555dcf8800a56dfadcb783ad17c55e
Hairo <Hairokirby21@gmail.com>
2020-07-24 01:20:54 +0000

    add the draw fire nation function

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ function setup() {
 
 function draw() {
   background(220);
-  circle(10, 10, 59);
+  drawFireNation();
+}
 
+function drawFireNation() {
+  noFill();
+  strokeWeight(5);
+  stroke(color(161, 0, 0));
+
+  triangle(200, 275, 220, 275, 210, 220);
+  triangle(280, 275, 300, 275, 290, 210);
+  triangle(230, 220, 100, 220, 200, 300)
+  ellipse(250, 250, 60, 60);
 }
